6978d62c7e50367b10fa8f7f65c720846ae66882
diamond <aaron.diamond2@yahoo.co.nz>
2023-01-09 00:06:30 +1300

    integrated api with the form

--- src/components/Form/Form.jsx
+++ src/components/Form/Form.jsx
@@ -1,16 +1,80 @@
 import React, { useState } from "react";
 import { doctordata } from "../../Mock/mockdoctors";
+import { API_URI } from "../../assets/js/Apiuri";
 
 import "../../styles/formstyles.css";
 const Form = () => {
   const [yesCondition, setYesCondition] = useState(false);
   const [yesAllergies, setYesAllergies] = useState(false);
 
+  const [firstName, setFirstName] = useState("");
+  const [lastName, setLastName] = useState("");
+  const [sex, setSex] = useState("");
+  const [nationality, setNationality] = useState("");
+  const [residencyStatus, setResidencyStatus] = useState("");
+  const [personalEmail, setPersonalEmail] = useState("");
+
+  const [address, setAddress] = useState("");
+  const [suburb, setSuburb] = useState("");
+  const [postCode, setPostCode] = useState("");
+  const [city, setCity] = useState("");
+  const [phoneNumber, setPhoneNumber] = useState("");
+
+  const [contactName, setContactName] = useState("");
+  const [contactNumber, setContactNumber] = useState("");
+  const [relationship, setRelationship] = useState("");
+  const [contactEmail, setContactEmail] = useState("");
+
+  const [generalPrac, setGeneralPrac] = useState("");
+
+  const [haveConditions, setHaveConditions] = useState("");
+  const [conditions, setConditions] = useState("");
+
+  const [haveAllergies, setHaveAllergies] = useState("");
+  const [allergies, setAllergies] = useState("");
+
+  const addData = (e) => {
+    e.preventDefault();
+    fetch(API_URI, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        Accept: "application/json",
+      },
+      body: JSON.stringify({
+        FirstName: firstName,
+        LastName: lastName,
+        Sex: sex,
+        Nationality: nationality,
+        ResidencyStatus: residencyStatus,
+        PersonalEmail: personalEmail,
+        ContactName: contactName,
+        EmailAddress: contactEmail,
+        ContactNumber: contactNumber,
+        Relationship: relationship,
+        GpName: generalPrac,
+        HaveUnderlyingConditions: haveConditions,
+        UnderlyingConditions: conditions,
+        HaveAllergies: haveAllergies,
+        Allergies: allergies,
+        StreetAddress: address,
+        Suburb: suburb,
+        PostCode: postCode,
+        City: city,
+        PhoneNumber: phoneNumber,
+      }),
+    })
+      .then((res) => console.log(res))
+      .then(() => {
+        window.location.reload();
+      });
+  };
+
   return (
     <div>
       <div>
         <h1 className="title">E-medical Form</h1>
-        <form className="form-container">
+        <form className="form-container" onSubmit={addData}>
           <div className="form-secondary-container">
             <div>
               <div className="form-divider">
@@ -18,14 +82,24 @@ const Form = () => {
                   First Name
                 </label>
                 <br />
-                <input className="form-input" type="text" id="fname" />
+                <input
+                  className="form-input"
+                  type="text"
+                  id="fname"
+                  onChange={(e) => setFirstName(e.target.value)}
+                />
               </div>
               <div className="form-divider">
                 <label className="form-label" htmlFor="lname">
                   Last Name
                 </label>
                 <br />
-                <input className="form-input" type="text" id="lname" />
+                <input
+                  className="form-input"
+                  type="text"
+                  id="lname"
+                  onChange={(e) => setLastName(e.target.value)}
+                />
               </div>
 
               <div className="form-divider">
@@ -33,7 +107,11 @@ const Form = () => {
                   Sex
                 </label>
                 <br />
-                <select className="form-input genderselection" id="gender">
+                <select
+                  className="form-input genderselection"
+                  id="gender"
+                  onChange={(e) => setSex(e.target.value)}
+                >
                   <option></option>
                   <option>M</option>
                   <option>F</option>
@@ -46,14 +124,23 @@ const Form = () => {
                   Nationality
                 </label>
                 <br />
-                <input className="form-input" type="text" id="nationality" />
+                <input
+                  className="form-input"
+                  type="text"
+                  id="nationality"
+                  onChange={(e) => setNationality(e.target.value)}
+                />
               </div>
               <div className="form-divider">
                 <label className="form-label" htmlFor="rstatus">
                   Residency Status
                 </label>
                 <br />
-                <select className="form-input" id="rstatus">
+                <select
+                  className="form-input"
+                  id="rstatus"
+                  onChange={(e) => setResidencyStatus(e.target.value)}
+                >
                   <option></option>
                   <option>Work Visa</option>
                   <option>Permanent Resident</option>
@@ -71,6 +158,7 @@ const Form = () => {
                   className="form-input personalemail"
                   type="text"
                   id="personalemail"
+                  onChange={(e) => setPersonalEmail(e.target.value)}
                 />
               </div>
             </div>
@@ -84,6 +172,7 @@ const Form = () => {
                   className="form-input address"
                   type="text"
                   id="address"
+                  onChange={(e) => setAddress(e.target.value)}
                 />
               </div>
               <div className="form-divider">
@@ -91,7 +180,12 @@ const Form = () => {
                   Suburb
                 </label>
                 <br />
-                <input className="form-input" type="text" id="suburb" />
+                <input
+                  className="form-input"
+                  type="text"
+                  id="suburb"
+                  onChange={(e) => setSuburb(e.target.value)}
+                />
               </div>
               <div className="postal-city-divider">
                 <div className="form-divider">
@@ -103,6 +197,7 @@ const Form = () => {
                     className="form-input postcode"
                     type="text"
                     id="postcode"
+                    onChange={(e) => setPostCode(e.target.value)}
                   />
                 </div>
 
@@ -111,7 +206,12 @@ const Form = () => {
                     City
                   </label>
                   <br />
-                  <input className="form-input city" type="text" id="city" />
+                  <input
+                    className="form-input city"
+                    type="text"
+                    id="city"
+                    onChange={(e) => setCity(e.target.value)}
+                  />
                 </div>
               </div>
               <div className="form-divider">
@@ -119,7 +219,12 @@ const Form = () => {
                   Mobile Number
                 </label>
                 <br />
-                <input className="form-input" type="text" id="mobilenum" />
+                <input
+                  className="form-input"
+                  type="text"
+                  id="mobilenum"
+                  onChange={(e) => setPhoneNumber(e.target.value)}
+                />
               </div>
             </div>
 
@@ -133,6 +238,7 @@ const Form = () => {
                   className="form-input emergencyname"
                   type="text"
                   id="eName"
+                  onChange={(e) => setContactName(e.target.value)}
                 />
               </div>
 
@@ -146,6 +252,7 @@ const Form = () => {
                     className="form-input emergencynumber"
                     type="text"
                     id="eNum"
+                    onChange={(e) => setContactNumber(e.target.value)}
                   />
                 </div>
 
@@ -161,6 +268,7 @@ const Form = () => {
                     className="form-input emergencyrelationship"
                     type="text"
                     id="erelation"
+                    onChange={(e) => setRelationship(e.target.value)}
                   />
                 </div>
               </div>
@@ -173,17 +281,22 @@ const Form = () => {
                   className="form-input emergencyname"
                   type="text"
                   id="e-email"
+                  onChange={(e) => setContactEmail(e.target.value)}
                 />
               </div>
             </div>
 
             <div className="GP-divider">
               <div className="form-divider">
-                <label htmlFor="eName" className="form-label">
+                <label htmlFor="gpName" className="form-label">
                   General Practitioner's Name
                 </label>
                 <br />
-                <select className="form-input gpselection">
+                <select
+                  className="form-input gpselection"
+                  id="gpName"
+                  onChange={(e) => setGeneralPrac(e.target.value)}
+                >
                   <option>None</option>
                   {doctordata.map((items) => (
                     <option key={items.id}>
@@ -198,9 +311,7 @@ const Form = () => {
           <div className="form-third-container">
             <div>
               <div>
-                <p htmlFor="med-condition">
-                  Do you have any underlying conditions?
-                </p>
+                <p>Do you have any underlying conditions?</p>
 
                 <label className="radio-label" htmlFor="yes-condition">
                   Yes
@@ -210,15 +321,16 @@ const Form = () => {
                   type="radio"
                   id="yes-condition"
                   value="true"
-                  onChange={() => setYesCondition(true)}
+                  onChange={(e) => setHaveConditions(e.target.value)}
                 />
                 <br />
 
-                {yesCondition === true ? (
+                {haveConditions === "true" ? (
                   <div>
                     <textarea
                       className="text-area"
                       placeholder="if 'yes', please explain your conditions"
+                      onChange={(e) => setConditions(e.target.value)}
                     ></textarea>
                     <br />
                   </div>
@@ -234,7 +346,7 @@ const Form = () => {
                   type="radio"
                   id="no-condition"
                   value="false"
-                  onChange={() => setYesCondition(false)}
+                  onChange={(e) => setHaveConditions(e.target.value)}
                 />
               </div>
 
@@ -249,15 +361,16 @@ const Form = () => {
                   type="radio"
                   id="yes-allergies"
                   value="true"
-                  onChange={() => setYesAllergies(true)}
+                  onChange={(e) => setHaveAllergies(e.target.value)}
                 />
                 <br />
 
-                {yesAllergies === true ? (
+                {haveAllergies === "true" ? (
                   <div>
                     <textarea
                       className="text-area"
                       placeholder="if 'yes', please list your allergies"
+                      onChange={(e) => setAllergies(e.target.value)}
                     ></textarea>
                     <br />
                   </div>
@@ -273,7 +386,7 @@ const Form = () => {
                   type="radio"
                   id="no-allergies"
                   value="false"
-                  onChange={() => setYesAllergies(false)}
+                  onChange={(e) => setHaveAllergies(e.target.value)}
                 />
               </div>
             </div>
